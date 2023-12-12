a68e7827efa12be7eb01a724b5991a6c29570315
guojw <2315162186@qq.com>
2023-12-12 16:29:18 +0800

    feat: init

---
+++ .stylelintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss"
+  ],
+  "rules": {
+    "declaration-empty-line-before": "never",
+    "unit-allowed-list": [
+      "em",
+      "rem"
+    ],
+    "selector-max-id": 1
+  }
+};
